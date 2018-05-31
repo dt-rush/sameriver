47d296fe8e58b8e99d23fa53d69538d21232beae
dt-rush <nick.eightfold.hunter@gmail.com>
2018-05-31 17:30:09 -0400

    finished generation of event_enum.go

--- testing/build/jen_gen/main.go
+++ testing/build/jen_gen/main.go
@@ -5,14 +5,16 @@ import (
 	"flag"
 	"fmt"
 	// "github.com/dave/jennifer/jen"
-	// "go/ast"
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"io/ioutil"
 	// "os"
 	"os/user"
 	"reflect"
+	"regexp"
 	"runtime"
+	"strings"
 )
 
 // helper function, thanks stack oveflow
@@ -54,6 +56,7 @@ func NewGenerateProcess(rootTargets TargetsCollection) *GenerateProcess {
 // used to run the targets and recursively run any more targets they produce
 func (g *GenerateProcess) runTargets(targets TargetsCollection) {
 	for target, f := range targets {
+		fmt.Printf("----- running target: %s -----\n", target)
 		message, err, moreTargets := f(target, g.fset)
 		g.messages[target] = message
 		g.errors[target] = fmt.Sprintf("%v", err)
@@ -145,6 +148,7 @@ func generateEventEnum(
 	target string, fset *token.FileSet) (
 	message string, err error, moreTargets TargetsCollection) {
 
+	// read the events.go file and get it as an ast.File
 	eventsDefFile := fmt.Sprintf("%s/events.go", *engineDir)
 	var buffer bytes.Buffer
 	eventsDefSrc, err := ioutil.ReadFile(eventsDefFile)
@@ -154,6 +158,65 @@ func generateEventEnum(
 	}
 	astFile, err := parser.ParseFile(fset, "",
 		eventsDefSrc, parser.AllErrors)
+	if err != nil {
+		msg := fmt.Sprintf("failed to generate astFile for %s",
+			eventsDefFile)
+		return msg, err, nil
+	}
+	// traverse the declarations in the ast.File to get the event names
+	eventNames := make([]string, 0)
+	for _, d := range astFile.Decls {
+		decl, ok := d.(*(ast.GenDecl))
+		if !ok {
+			continue
+		}
+		if decl.Tok != token.TYPE {
+			continue
+		}
+		name := decl.Specs[0].(*ast.TypeSpec).Name.Name
+		if validName, _ := regexp.MatchString(".+Event", name); !validName {
+			fmt.Printf("type %s in %s does not match regexp for an event "+
+				"type (\".+Event\"). Will not include in generated files.\n",
+				name, eventsDefFile)
+			continue
+		}
+		eventNames = append(eventNames, name)
+		fmt.Printf("found event: %+v\n", name)
+	}
+	// generate the source file
+	constNames := make(map[string]string)
+	buffer.WriteString(`// THIS FILE IS GENERATED
+package engine
+
+type EventType int
+
+`)
+	buffer.WriteString(fmt.Sprintf(
+		"const N_EVENT_TYPES = %d\n\n", len(eventNames)))
+	buffer.WriteString("const (\n")
+	for _, eventName := range eventNames {
+		eventNameStem := strings.Replace(eventName, "Event", "", 1)
+		constNames[eventName] = strings.ToUpper(eventNameStem) + "_EVENT"
+		buffer.WriteString(fmt.Sprintf(
+			"\t%s = EventType(iota)\n",
+			constNames[eventName]))
+	}
+	buffer.WriteString(")\n\n")
+	buffer.WriteString("var EVENT_NAMES = map[EventType]string{\n")
+	for _, eventName := range eventNames {
+		buffer.WriteString(fmt.Sprintf(
+			"\t%s: \"%s\",\n",
+			constNames[eventName],
+			constNames[eventName]))
+	}
+	buffer.WriteString("}")
+	fmt.Printf("================")
+	fmt.Printf("GENERATED SOURCE")
+	fmt.Printf("================\n\n")
+	fmt.Println(buffer.String())
+	fmt.Printf("================")
+	fmt.Printf("================")
+	fmt.Printf("================\n\n")
 	return "generated", nil, nil
 }
 
@@ -167,7 +230,7 @@ func main() {
 		homedir := usr.HomeDir
 		*engineDir = fmt.Sprintf("%s/go/src/github.com/dt-rush/donkeys-qquest/engine", homedir)
 	}
-	fmt.Printf("Generating code from engine dir: %s ...\n",
+	fmt.Printf("Engine dir is: %s\n",
 		*engineDir)
 
 	var ROOT_TARGETS = TargetsCollection{
