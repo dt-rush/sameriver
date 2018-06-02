ec6fade0fef579d9b08e4735cb02cadc49c9744b
dt-rush <nick.eightfold.hunter@gmail.com>
2018-06-01 22:04:29 -0400

    continued to tighten up build script, defined TODO file for path forward

--- sameriver-build.go
+++ sameriver-build.go
@@ -10,6 +10,7 @@ import (
 )
 
 const DEFAULT_ENGINE_DIR = "$GOPATH/src/github.com/dt-rush/sameriver/engine"
+const DEFAULT_OUTPUT_DIR = "/tmp/sameriver"
 
 var engineDir = flag.String("enginedir",
 	DEFAULT_ENGINE_DIR,
@@ -17,6 +18,9 @@ var engineDir = flag.String("enginedir",
 var gameDir = flag.String("gamedir",
 	"",
 	"the game/ directory of the game we're generating engine source files for")
+var outputDir = flag.String("outputdir",
+	DEFAULT_OUTPUT_DIR,
+	"the directory in which to output the generated files")
 
 func pathResolve(path string) string {
 	resolved := path
@@ -42,17 +46,22 @@ func main() {
 	} else {
 		*gameDir = pathResolve(*gameDir)
 	}
+	if *outputDir == DEFAULT_OUTPUT_DIR {
+		os.MkdirAll(DEFAULT_OUTPUT_DIR, os.ModePerm)
+	}
 	fmt.Printf("sameriver/engine/ dir is: %s\n", *engineDir)
 	fmt.Printf("${yourgame}/game/ dir is: %s\n", *gameDir)
 
-	g := build.NewGenerateProcess(*engineDir, *gameDir)
+	g := build.NewGenerateProcess(*engineDir, *gameDir, *outputDir)
 	g.Run(build.TargetsCollection{
 		"events":     g.GenerateEventFiles,
 		"components": g.GenerateComponentFiles,
 	})
 	g.PrintReport()
-	g.PrintSourceFiles()
 	if g.HadErrors() {
+		fmt.Println("Errors were encountered, exiting 1")
 		os.Exit(1)
+	} else {
+		g.OutputFiles()
 	}
 }
