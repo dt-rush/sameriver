c3ea7a86a6ce76c0d1b9070a5be82f4aa0d52975
dt-rush <nick.8.payne@gmail.com>
2018-07-07 13:01:28 -0400

    modified entity_class to add method Entities() which should return a list of tokens

--- engine/entity_class.go
+++ engine/entity_class.go
@@ -1,12 +1,9 @@
-/*
- * EntityClass is used to group a set of entities by the EntityManager,
- * and to allow classes of entities (eg. "bear", "crow") to define themselves
- *
- */
-
+// EntityClass is used to group a set of entities by the EntityManager,
+// and to allow classes of entities (eg. "bear", "crow") to define themselves
 package engine
 
 type EntityClass interface {
 	Name() string
 	DefaultSpawnRequest(position [2]int32) SpawnRequestData
+	Entities() []*EntityToken
 }
