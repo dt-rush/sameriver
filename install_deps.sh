a1ad215781172387a7f101a4c8ade06e0d77cfd4
dt-rush <nick.8.payne@gmail.com>
2018-07-11 23:20:37 +0000

    improved install_deps.sh a lot to be more robust, allowing it to extent to other systems

--- install_deps.sh
+++ install_deps.sh
@@ -1,11 +1,65 @@
 #!/bin/bash
 
-if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
-	echo "running apt-get update && apt-get install libsdl2{,-mixer,-image,-ttf,-gfx}-dev"
+needed=()
+
+do_pkg_check() {
+	for package in "${packages[@]}"; do
+		printf "checking for $package ... "
+		if [ "$(($1) | grep $package)" ]; then
+			echo "installed"
+			continue
+		else
+			echo "not installed"
+			needed+=($package)
+		fi
+	done
+}
+
+apt_check() {
+	packages=("libsdl2-dev" \
+		"libsdl2-mixer-dev" \
+		"libsdl2-image-dev" \
+		"libsdl2-ttf-dev" \
+		"libsdl2-gfx-dev")
+	do_pkg_check "dpkg -l"
+}
+
+apt_install() {
 	sudo apt-get update && \
 	sudo apt-get install libsdl2{,-mixer,-image,-ttf,-gfx}-dev
-elif [ "$(grep -i 'arch' /etc/issue)" ]; then
-	echo "running pacman -Sy && pacman -S sdl2{,_mixer,_image,_ttf,_gfx}"
+}
+
+pacman_check() {
+	packages=("sdl2" \
+		"sdl_mixer" \
+		"sdl_image" \
+		"sdl_ttf" \
+		"sdl_gfx")
+	do_pkg_check "pacman -Qs"
+}
+
+pacman_install() {
 	sudo pacman -Sy && \
 	sudo pacman -S sdl2{,_mixer,_image,_ttf,_gfx}
+}
+
+detect_pkgsystem() {
+	if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+		pkg_check="apt_check"
+		pkg_install="apt_install"
+	elif [ "$(grep -i 'arch' /etc/issue)" ]; then
+		pkg_check="pacman_check"
+		pkg_install="pacman_install"
+	fi
+}
+
+detect_pkgsystem
+($pkg_check)
+if [ ${#needed[@]} -ne 0 ]; then
+	echo "######################################################"
+	echo -e "need to install:\n\n${needed[@]}\n"
+	echo "######################################################"
+	($pkg_install)
+else
+	echo "all needed apt packages installed"
 fi
