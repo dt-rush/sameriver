209e38ad990e8b9ab14778a15998956537c55484
dt-rush <nick.8.payne@gmail.com>
2018-07-12 08:49:25 -0400

    improved install_godeps.sh and slightly install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -4,19 +4,20 @@ needed=()
 
 do_pkg_check() {
 	for package in "${packages[@]}"; do
-		printf "checking for $package ... "
-		if [ "$(($1) | grep $package)" ]; then
+		printf "checking for %s ... " "${package}"
+		if ($1) | grep -q "${package}"; then
 			echo "installed"
 			continue
 		else
-			echo "not installed"
+			echo "-------- not installed"
 			needed+=($package)
 		fi
 	done
 }
 
 apt_check() {
-	packages=("libsdl2-dev" \
+	packages=(\
+		"libsdl2-dev" \
 		"libsdl2-mixer-dev" \
 		"libsdl2-image-dev" \
 		"libsdl2-ttf-dev" \
@@ -26,11 +27,12 @@ apt_check() {
 
 apt_install() {
 	sudo apt-get update && \
-	sudo apt-get install ${needed[@]}
+	sudo apt-get install "${needed[@]}"
 }
 
 pacman_check() {
-	packages=("sdl2" \
+	packages=(\
+		"sdl2" \
 		"sdl2_mixer" \
 		"sdl2_image" \
 		"sdl2_ttf" \
@@ -40,14 +42,14 @@ pacman_check() {
 
 pacman_install() {
 	sudo pacman -Sy && \
-	sudo pacman -S ${needed[@]}
+	sudo pacman -S "${needed[@]}"
 }
 
 detect_pkgsystem() {
-	if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+	if grep -q -Ei 'debian|buntu|mint' /etc/*release; then
 		pkg_check="apt_check"
 		pkg_install="apt_install"
-	elif [ "$(grep -i 'arch' /etc/issue)" ]; then
+	elif grep -q -i 'arch' /etc/issue; then
 		pkg_check="pacman_check"
 		pkg_install="pacman_install"
 	fi
@@ -57,7 +59,7 @@ detect_pkgsystem
 eval $pkg_check
 if [ ${#needed[@]} -ne 0 ]; then
 	echo "######################################################"
-	echo -e "need to install:\n\n${needed[@]}\n"
+	echo -e "need to install:\n\n" "${needed[@]}" "\n"
 	echo "######################################################"
 	set -x
 	echo "\$pkg_install is $pkg_install"
--- install_godeps.sh
+++ install_godeps.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
 
-go get "github.com/golang-collections/go-datastructures/bitarray"
-go get "github.com/veandco/go-sdl2/img"
-go get "github.com/veandco/go-sdl2/mix"
-go get "github.com/veandco/go-sdl2/sdl"
-go get "github.com/veandco/go-sdl2/ttf"
-go get "github.com/dave/jennifer"
-go get "go.uber.org/atomic"
+needed=()
+
+packages=(\
+"github.com/golang-collections/go-datastructures/bitarray" \
+"github.com/veandco/go-sdl2/img" \
+"github.com/veandco/go-sdl2/mix" \
+"github.com/veandco/go-sdl2/sdl" \
+"github.com/veandco/go-sdl2/ttf" \
+"github.com/dave/jennifer" \
+"go.uber.org/atomic")
+
+do_pkg_check() {
+	installed="$(go list '...' 2>/dev/null)"
+	if [[ -z ${installed} ]]; then
+		go list '...'
+		exit 1
+	fi
+	for package in "${packages[@]}"; do
+		printf "checking for %s ... " "${package}"
+		if echo "${installed}" | grep -q "${package}"; then
+			echo "installed"
+			continue
+		else
+			echo "-------- not installed"
+			needed+=($package)
+		fi
+	done
+}
+
+install() {
+	go get -v "${needed[@]}"	
+}
+
+do_pkg_check
+if [ ${#needed[@]} -ne 0 ]; then
+	echo "######################################################"
+	echo -e "need to install:\n\n" "${needed[@]}" "\n"
+	echo "######################################################"
+	set -x
+	install
+else
+	echo "all needed go packages installed"
+fi
