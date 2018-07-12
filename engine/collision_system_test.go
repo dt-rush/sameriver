9b21328c568d44b39e9df7d43366de083f2d38ac
dt-rush <nick.8.payne@gmail.com>
2018-07-12 12:00:12 -0400

    added test for collision rate limiter reset

--- engine/collision_system_test.go
+++ engine/collision_system_test.go
@@ -40,7 +40,7 @@ func TestCollisionRateLimit(t *testing.T) {
 	ec := w.ev.Subscribe(
 		"SimpleCollisionQuery",
 		NewSimpleEventQuery(COLLISION_EVENT))
-	w.em.Spawn(collisionSpawnRequestData())
+	e, _ := w.em.Spawn(collisionSpawnRequestData())
 	w.em.Spawn(collisionSpawnRequestData())
 	w.em.Update()
 	w.Update(FRAME_SLEEP_MS)
@@ -49,4 +49,12 @@ func TestCollisionRateLimit(t *testing.T) {
 	if len(ec.C) != 1 {
 		t.Fatal("collision rate-limiter didn't prevent collision duplication")
 	}
+	w.Update(FRAME_SLEEP_MS)
+	cs.rateLimiterArray.Reset(e)
+	w.Update(FRAME_SLEEP_MS)
+	time.Sleep(FRAME_SLEEP)
+	if len(ec.C) != 2 {
+		t.Fatal("collision rate-limiter reset did not allow second collision")
+	}
+
 }
