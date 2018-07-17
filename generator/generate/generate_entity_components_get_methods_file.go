689b4fb161a00c8e8faf91f497c1d4716eae3678
dt-rush <nick.8.payne@gmail.com>
2018-07-17 18:53:56 +0000

    changed generation of get methods

--- generator/generate/generate_entity_components_get_methods_file.go
+++ generator/generate/generate_entity_components_get_methods_file.go
@@ -14,20 +14,6 @@ func generateEntityComponentsGetMethodsFile(components []ComponentSpec) *File {
 		f.Func().
 			Params(Id("e").Op("*").Id("Entity")).
 			Id(fmt.Sprintf("Get%s", component.Name)).Params().
-			Id(component.Type).
-			Block(
-				Return(
-					Id("e").Dot("World").Dot("Components").
-						Dot(component.Name).Index(Id("e").Dot("ID")),
-				),
-			)
-	}
-
-	// generate GetRef methods
-	for _, component := range components {
-		f.Func().
-			Params(Id("e").Op("*").Id("Entity")).
-			Id(fmt.Sprintf("Get%sRef", component.Name)).Params().
 			Op("*").Id(component.Type).
 			Block(
 				Return(
