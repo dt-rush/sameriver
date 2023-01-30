9d60ce488774ecf63cbb2a9a42e829937c15a398
npayne <nick.payne@anuvu.com>
2023-01-30 13:19:19 -0500

    CCC BitArray presence test

--- engine/custom_contiguous_component_test.go
+++ engine/custom_contiguous_component_test.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"regexp"
 	"testing"
 )
 
@@ -61,5 +62,31 @@ func TestCCCGetSet(t *testing.T) {
 	expected = XYZ{x: 3, y: 3, z: 3}
 	if secondGet != expected {
 		t.Errorf("Didn't Set() properly")
+	}
+}
+
+func TestCCCBitArray(t *testing.T) {
+	w := testingWorld()
+	xyz := &XYZComponent{}
+	w.RegisterCCCs([]CustomContiguousComponent{
+		xyz,
+	})
+	// spawn entity with empty base CS, XYZ custom CS
+	basicSpecs := map[string]interface{}{}
+	cccSpecs := map[string]interface{}{
+		"Custom,XYZ": XYZ{x: 1, y: 0, z: 8},
+	}
+	cccCS := MakeCustomComponentSet(
+		basicSpecs,
+		cccSpecs,
+		map[string]CustomContiguousComponent{
+			"XYZ": xyz,
+		})
+	e, _ := w.em.Spawn([]string{}, cccCS)
+	b := e.ComponentBitArray
+	s := w.em.components.BitArrayToString(b)
+	// TODO: test string for XYZ
+	if valid, _ := regexp.MatchString("XYZ", s); !valid {
+		t.Errorf("XYZ not in component bit array -> string")
 	}
 }
