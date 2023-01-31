b7702fedda53a92e2d78d5682bbef5144a55c8a2
npayne <nick.payne@anuvu.com>
2023-01-31 16:21:23 -0500

    DIE BUG DIE

--- v2/utils/time_accumulator.go
+++ v2/utils/time_accumulator.go
@@ -1,27 +1,28 @@
 package utils
 
 type TimeAccumulator struct {
-	accum     float64
+	accum_ms  float64
 	period_ms float64
 }
 
 func NewTimeAccumulator(period_ms float64) TimeAccumulator {
 	t := TimeAccumulator{}
-	t.accum = 0
+	t.accum_ms = 0
 	t.period_ms = period_ms
 	return t
 }
 
 func (t *TimeAccumulator) Tick(dt_ms float64) bool {
-	t.accum += dt_ms
+	Logger.Printf("t.accum_ms: %f, t.period: %f, dt_ms: %f", t.accum_ms, t.period_ms, dt_ms)
+	t.accum_ms += dt_ms
 	had_tick := false
-	for t.accum >= t.period_ms {
-		t.accum -= t.period_ms
+	for t.accum_ms >= t.period_ms {
+		t.accum_ms -= t.period_ms
 		had_tick = true
 	}
 	return had_tick
 }
 
 func (t *TimeAccumulator) Completion() float64 {
-	return t.accum / t.period_ms
+	return t.accum_ms / t.period_ms
 }
