3602d1de753cb0b38acab98552a02c80de88500f
npayne <nick.payne@anuvu.com>
2023-01-31 16:17:21 -0500

    WOW the bug that won't die

--- v2/runtime_limiter.go
+++ v2/runtime_limiter.go
@@ -74,10 +74,12 @@ func (r *RuntimeLimiter) Run(allowance_ms float64) (remaining_ms float64) {
 			(hasEstimate && estimate > allowance_ms && r.runIX == r.startIX) {
 			t0 = time.Now()
 			dt_ms := float64(time.Since(logic.lastRun).Nanoseconds()) / 1.0e6
+			Logger.Printf("%s has runSchedule: %v", logic.name, logic.runSchedule)
 			if logic.active && (logic.runSchedule == nil || logic.runSchedule.Tick(dt_ms)) {
 				logic.f(dt_ms)
 				logic.lastRun = time.Now()
 			}
+			Logger.Printf("%s complete.", logic.name)
 			elapsed_ms = float64(time.Since(t0).Nanoseconds()) / 1.0e6
 			// update estimate stat
 			if !hasEstimate {
