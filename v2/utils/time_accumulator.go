e853c1a4c76c2c8e883f3d3bda086a8606c966ec
npayne <nick.payne@anuvu.com>
2023-01-31 15:53:30 -0500

    still debugging TimeAccumulator

--- v2/utils/time_accumulator.go
+++ v2/utils/time_accumulator.go
@@ -12,13 +12,16 @@ func NewTimeAccumulator(period_ms float64) TimeAccumulator {
 	return t
 }
 
-func (t *TimeAccumulator) Tick(dt float64) bool {
+func (t *TimeAccumulator) Tick(dt_ms float64) bool {
 	Logger.Println("In Tick()")
-	t.accum += dt
+	t.accum += dt_ms
 	had_tick := false
+	Logger.Printf("t.accum: %f", t.accum)
+	Logger.Printf("t.period_ms: %f", t.period_ms)
 	for t.accum >= t.period_ms {
 		Logger.Println("t.accum >= t.period_ms")
 		t.accum -= t.period_ms
+		Logger.Printf("after sub: t.accum: %f", t.accum)
 		had_tick = true
 	}
 	return had_tick
