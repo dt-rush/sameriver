b8d25e0ddde04b1784a20f58573ca8003372180a
npayne <nick.payne@anuvu.com>
2023-02-01 10:56:11 -0500

    revert to non-goroutine collision and spatial hash system

--- v2/collision_system.go
+++ v2/collision_system.go
@@ -33,19 +33,12 @@
 package sameriver
 
 import (
-	"runtime"
-	"sync"
 	"time"
 )
 
 type CollisionData struct {
 	This  *Entity
 	Other *Entity
-}
-
-type EntityPair struct {
-	A *Entity
-	B *Entity
 }
 
 type CollisionSystem struct {
@@ -115,23 +108,8 @@ func (s *CollisionSystem) checkEntities(entities []*Entity) {
 	// table might have been despawned since the last time a spatial hash
 	// was computed (not every system is guaranteed to run every update loop,
 	// so maybe spatial hash didn't run but an entity or world logic did, to
-	// despawn one of the *Entity objects still stored in the last-computed
-	// spatial hash table).
-	var wg sync.WaitGroup
-	entitiesCh := make(chan *EntityPair)
-
-	workers := runtime.NumCPU()
-
-	// launch workers
-	for i := 0; i < workers; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			s.checkEntityWorker(entitiesCh)
-		}()
-	}
-
-	// send work to workers
+	// despawn one of the tokens still stored in the last-computed spatial hash
+	// table).
 	for ix := 0; ix < len(entities); ix++ {
 		i := entities[ix]
 		if i.Despawned {
@@ -146,24 +124,10 @@ func (s *CollisionSystem) checkEntities(entities []*Entity) {
 			if j.ID < i.ID {
 				j, i = i, j
 			}
-			entitiesCh <- &EntityPair{i, j}
-		}
-	}
-
-	close(entitiesCh)
-	wg.Wait()
-}
-
-func (s *CollisionSystem) checkEntityWorker(entitiesCh chan *EntityPair) {
-	for {
-		entityPair := <-entitiesCh
-		if entityPair == nil {
-			return
-		}
-		i, j := entityPair.A, entityPair.B
-		if !s.rateLimiterArray.GetRateLimiter(i.ID, j.ID).Limited() &&
-			s.TestCollision(i, j) {
-			s.DoCollide(i, j)
+			if !s.rateLimiterArray.GetRateLimiter(i.ID, j.ID).Limited() &&
+				s.TestCollision(i, j) {
+				s.DoCollide(i, j)
+			}
 		}
 	}
 }
