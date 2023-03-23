4bc30b92fe323661faba31d9276f09cc81f102d0
npayne <nick.payne@anuvu.com>
2023-03-23 17:39:30 -0400

    started to transfer the curves in from desmos

--- v4/curves.go
+++ v4/curves.go
@@ -13,14 +13,50 @@ var Curves = curves{}
 
 type CurveFunc func(x float64) float64
 
+//
+// CLIMB
+//
+
+func (c *curves) Sigmoid(s float64) CurveFunc {
+	return func(x float64) float64 {
+		denominator := 1.0 + math.Exp(-x/(0.05*s))
+		return math.Pow(1.0/denominator, math.Abs(x-1))
+	}
+}
+
+func (c *curves) Shelf(u float64, s float64) CurveFunc {
+	n0 := func(x0, s float64) float64 {
+		numerator := math.Pow(2-x0, 10*s)
+		denominator := math.Pow(2-x0, 10*s) - 1
+		return numerator / denominator
+	}
+	return func(x float64) float64 {
+		if x == 1 {
+			return 1
+		}
+		return math.Max(0, n0(u, s)*(1-math.Pow((x-u)+1, -10*s)))
+	}
+}
+
+func (c *curves) Arc(u float64, s float64) CurveFunc {
+	return func(x float64) float64 {
+		if x == 1 {
+			return 1
+		}
+		if x >= u {
+			numerator := math.Sqrt(x - u)
+			denominator := math.Sqrt(1 - u)
+			return math.Pow(numerator/denominator, 1/s)
+		} else {
+			return 0
+		}
+	}
+}
+
 func (c *curves) Bell(spread float64) CurveFunc {
 	return func(x float64) float64 {
 		return 1.0 / ((x*x)/(spread*spread) + 1)
 	}
-}
-
-func (c *curves) Sigmoid(x float64) float64 {
-	return 1.0 / (1 + math.Exp(-x/0.2))
 }
 
 func (c *curves) Quadratic(x float64) float64 {
