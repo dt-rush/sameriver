19293362ad12666edd007e1701e0f2717ad1eb79
npayne <nick.payne@anuvu.com>
2023-03-23 21:47:00 -0400

    some more curves

--- v4/curves.go
+++ v4/curves.go
@@ -20,16 +20,16 @@ type CurveFunc func(x float64) float64
 func (c *curves) Sigmoid(u float64, s float64) CurveFunc {
 	return func(x float64) float64 {
 		x = c.Clamped(x)
-		denominator := 1.0 + math.Exp(-(x-u)/(0.05*s))
-		return math.Pow(1.0/denominator, math.Abs((x-u)-1))
+		denom := 1.0 + math.Exp(-(x-u)/(0.05*s))
+		return math.Pow(1.0/denom, math.Abs((x-u)-1))
 	}
 }
 
 func (c *curves) Shelf(u float64, s float64) CurveFunc {
 	n0 := func(x0, s float64) float64 {
-		numerator := math.Pow(2-x0, 10*s)
-		denominator := math.Pow(2-x0, 10*s) - 1
-		return numerator / denominator
+		num := math.Pow(2-x0, 10*s)
+		denom := math.Pow(2-x0, 10*s) - 1
+		return num / denom
 	}
 	return func(x float64) float64 {
 		x = c.Clamped(x)
@@ -47,44 +47,112 @@ func (c *curves) Arc(u float64, s float64) CurveFunc {
 			return 1
 		}
 		if x >= u {
-			numerator := math.Sqrt(x - u)
-			denominator := math.Sqrt(1 - u)
-			return math.Pow(numerator/denominator, 1/s)
+			num := math.Sqrt(x - u)
+			denom := math.Sqrt(1 - u)
+			return math.Pow(num/denom, 1/s)
 		} else {
 			return 0
 		}
 	}
 }
 
-func (c *curves) Bell(s float64) CurveFunc {
+func (c *curves) Quad(u float64, k float64) CurveFunc {
 	return func(x float64) float64 {
-		return 1.0 / ((x*x)/(s*s) + 1)
+		x = c.Clamped(x)
+		if x == 1 {
+			return 1
+		}
+		if x <= u {
+			return 0
+		} else {
+			return math.Pow(x-u, 2*k) / math.Pow(1-u, 2*k)
+		}
 	}
 }
 
-func (c *curves) Quadratic(x float64) float64 {
-	return x * x
+func (c *curves) Cubi(u float64, k float64) CurveFunc {
+	return func(x float64) float64 {
+		x = c.Clamped(x)
+		if x == 1 {
+			return 1
+		}
+		if x <= u {
+			return 0
+		} else {
+			return (math.Pow((2/(1-u))*(x-u)-1, 3*k) + 1) / 2
+		}
+	}
 }
 
-func (c *curves) Linear(x float64) float64 {
-	return (x + 1) / 2
+func (c *curves) Lin(x float64) float64 {
+	x = c.Clamped(x)
+	return x
 }
 
-func (c *curves) Greater(b float64) CurveFunc {
+func (c *curves) Lint(u float64, w float64) CurveFunc {
+	return func(x float64) float64 {
+		x = c.Clamped(x)
+		return math.Min(1, math.Max(0, (x-u+w)/(2*w)))
+	}
+}
+
+func (c *curves) Exp(k float64) CurveFunc {
+	return func(x float64) float64 {
+		x = c.Clamped(x)
+		num := math.Exp(k*x) - 1
+		denom := math.Exp(k) - 1
+		return num / denom
+	}
+}
+
+//
+// INTERVALS
+//
+
+func (c *curves) Gt(b float64) CurveFunc {
 	return func(x float64) float64 {
 		return math.Min(1, math.Max(0, math.Ceil(x-b)))
 	}
 }
 
-func (c *curves) Less(b float64) CurveFunc {
+func (c *curves) Ge(b float64) CurveFunc {
+	return func(x float64) float64 {
+		return 1 - c.Lt(b)(x)
+	}
+}
+
+func (c *curves) Lt(b float64) CurveFunc {
 	return func(x float64) float64 {
 		return math.Min(1, math.Max(0, math.Ceil(b-x)))
 	}
 }
 
-func (c *curves) Interval(a, b float64) CurveFunc {
+func (c *curves) Le(b float64) CurveFunc {
 	return func(x float64) float64 {
-		return c.Greater(a)(x) + c.Less(b)(x) - 1
+		return 1 - c.Gt(b)(x)
+	}
+}
+
+// = 1 between a, b inclusive
+// = 0 elsewhere
+func (c *curves) Span(a, b float64) CurveFunc {
+	return func(x float64) float64 {
+		return c.Ge(a)(x) + c.Le(b)(x) - 1
+	}
+}
+
+// PEAKS
+func (c *curves) Bell(u float64, s float64) CurveFunc {
+	return func(x float64) float64 {
+		x = c.Clamped(x)
+		return 1.0 / ((x-u)*(x-u)/(s*s/64) + 1)
+	}
+}
+
+func (c *curves) BellPinned(u float64) CurveFunc {
+	return func(x float64) float64 {
+		x = c.Clamped(x)
+		return math.Max(0, -16*math.Pow(x-(u-0.5), 3)*4*math.Pow((x-(u-0.5))-1, 3))
 	}
 }
 
--- v4/curves_test.go
+++ v4/curves_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestCurves(t *testing.T) {
+func TestCurvesClimb(t *testing.T) {
 	expect := []float64{
 		Curves.Sigmoid(0.5, 1)(-1), 0,
 		Curves.Sigmoid(0.5, 1)(0), 0,
@@ -24,11 +24,58 @@ func TestCurves(t *testing.T) {
 		Curves.Shelf(1, 1)(0.5), 0,
 		Curves.Shelf(1, 1)(1), 1,
 		Curves.Shelf(1, 1)(2), 1,
+
+		Curves.Quad(0.5, 1)(-1), 0,
+		Curves.Quad(0.5, 1)(0), 0,
+		Curves.Quad(0.5, 1)(0.5), 0,
+		Curves.Quad(0.5, 1)(1), 1,
+		Curves.Quad(0.5, 1)(2), 1,
+
+		Curves.Cubi(0.5, 1)(-1), 0,
+		Curves.Cubi(0.5, 1)(0), 0,
+		Curves.Cubi(0.5, 1)(0.5), 0,
+		Curves.Cubi(0.5, 1)(1), 1,
+		Curves.Cubi(0.5, 1)(2), 1,
+
+		Curves.Lin(-1), 0,
+		Curves.Lin(0), 0,
+		Curves.Lin(0.5), 0.5,
+		Curves.Lin(1), 1,
+		Curves.Lin(2), 1,
+
+		Curves.Lint(0.5, 0.2)(-1), 0,
+		Curves.Lint(0.5, 0.2)(0), 0,
+		Curves.Lint(0.5, 0.2)(0.5), 0.5,
+		Curves.Lint(0.5, 0.2)(1), 1,
+		Curves.Lint(0.5, 0.2)(2), 1,
+
+		Curves.Exp(5)(-1), 0,
+		Curves.Exp(5)(0), 0,
+		Curves.Exp(5)(1), 1,
+		Curves.Exp(5)(2), 1,
 	}
+	Logger.Println(Curves.Lin(0.5))
 	for i := 0; i < len(expect); i += 2 {
 		// "close enough" since for example sigmoid(0.5, 1)(1) isn't exactly 1
 		if math.Abs(expect[i]-expect[i+1]) > 0.001 {
-			t.Fatalf("condition %d resulted in %f, not %f", i/2, expect[i/2], expect[i])
+			t.Fatalf("condition %d resulted in %f, not %f", i/2, expect[i], expect[i+1])
+		}
+	}
+}
+
+func TestCurvesPeaks(t *testing.T) {
+	expect := []float64{
+		Curves.Bell(0.5, 1)(-1), 0.058,
+		Curves.Bell(0.5, 1)(0), 0.058,
+		Curves.Bell(0.5, 1)(0.5), 1,
+		Curves.Bell(0.5, 1)(1), 0.058,
+		Curves.Bell(0.5, 1)(2), 0.058,
+	}
+	Logger.Println(Curves.Lin(0.5))
+	for i := 0; i < len(expect); i += 2 {
+		// "close enough" since for example sigmoid(0.5, 1)(1) isn't exactly 1
+		if math.Abs(expect[i]-expect[i+1]) > 0.001 {
+			t.Fatalf("condition %d resulted in %f, not %f", i/2, expect[i], expect[i+1])
 		}
 	}
 }
