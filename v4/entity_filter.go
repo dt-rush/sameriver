80c57e5fc861508f89db3cf84dea546d57f00318
npayne <nick.payne@anuvu.com>
2023-04-05 19:45:40 -0400

    halfway thru a grand idea

--- v4/entity_filter.go
+++ v4/entity_filter.go
@@ -39,3 +39,31 @@ func EntityFilterFromComponentBitArray(
 			return q.Equals(q.And(b))
 		}}
 }
+
+func EntityFilterFromCanBe(canBe map[string]int) EntityFilter {
+	return EntityFilter{
+		Name: "canbe",
+		Predicate: func(e *Entity) bool {
+			for k, v := range canBe {
+				if !e.GetIntMap(STATE).ValCanBeSetTo(k, v) {
+					return false
+				}
+			}
+			return true
+		},
+	}
+}
+
+// bit of a meta filter:
+// matches the closest entity to to that fulfills the given filter
+func EntityFilterFromClosest(to *Entity, filter EntityFilter) EntityFilter {
+	return EntityFilter{
+		Name: "closest",
+		Predicate: func(e *Entity) bool {
+			return e == to.World.ClosestEntityFilter(
+				*to.GetVec2D(POSITION),
+				*to.GetVec2D(BOX),
+				filter.Predicate)
+		},
+	}
+}
